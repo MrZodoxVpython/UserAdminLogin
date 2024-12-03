94d9804c7ec0855f4762fce70dfc5e2252ef440f
MrZodoxVpython <mrzodoxvpython@gmail.com>
2024-12-03 22:50:24 +0700

    Improve code

--- user.py
+++ user.py
@@ -8,9 +8,11 @@ def loginUser():
         p = len(userData[username])
         if password in userData[username] and len(password) == p:
             print("Login berhasil")
+            return True
         else:
             print("Password salah")
+            return False
     else:
         print("User tidak ada di database")
 def userDashboard():
-    pass
+    print("Welcome to user dashboard")
