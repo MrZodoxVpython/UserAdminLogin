442c1acc269134564f327aba5be2c53e1d3105ab
MrZodoxVpython <mrzodoxvpython@gmail.com>
2024-12-03 22:51:03 +0700

    adding verification code

--- login.py
+++ login.py
@@ -3,8 +3,6 @@ from admin import loginAdmin, adminDashboard
 def loginUserVerification():
     if loginUser():
         userDashboard()
-    else:
-        print("Login gagal")
 def loginAdminVerification():
     if loginAdmin():
         adminDashboard()
